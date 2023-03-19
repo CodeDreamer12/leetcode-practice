656ef356463e2d9acb2754968089dabfb583cea1
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2023-03-19 16:47:45 +0530

    Create Remove Linked List Elements.java

---
+++ Data Structure I/Day 7/Remove Linked List Elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+       ListNode curr=head;
+       ListNode prev=null;
+       while(curr!=null){
+           if(curr.val ==  val){
+               if(curr == head){
+                   head=head.next;
+               }
+               else{
+                   prev.next=curr.next;
+                   curr=prev;
+               }
+           }
+           prev=curr;
+           curr=curr.next;
+       }
+       return head;
+    }
+}
