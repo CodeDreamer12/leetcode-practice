da81eac5bcc52eece376cd9dfcca9a62844ce5d9
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2023-03-20 21:58:36 +0530

    Create Binary Tree Postorder Traversal.java

---
+++ Data Structure I/Day 10/Binary Tree Postorder Traversal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void postOrder(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
+        }
+        postOrder(root.left,list);
+        postOrder(root.right,list);
+        list.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new LinkedList<>();
+        postOrder(root,list);
+        return list;
+        
+    }
+}
